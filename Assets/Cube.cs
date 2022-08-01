30ef2854d1947527259efaeca85a01b859c1f493
diotsani <dio.tsani123@gmail.com>
2022-08-01 19:43:02 +0700

    up
    up

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -7,13 +7,84 @@ public class Cube : MonoBehaviour
     public float x;
     public float y;
     public float z;
+
+    public Rigidbody rb;
+    public bool dragging = false;
+    public float rotationSpeed;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         transform.Rotate(x, y, z * Time.deltaTime);
+        Drag();
+    }
+
+    private void FixedUpdate()
+    {
+        if (dragging)
+        {
+            float x = Input.GetAxis("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+            float y = Input.GetAxis("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+
+            rb.AddTorque(Vector3.down * x);
+            rb.AddTorque(Vector3.right * y);
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        dragging = true;
+    }
+
+    public void Controller()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Ground")
+                {
+                    dragging = true;
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Ground")
+                {
+                    dragging = true;
+                }
+            }
+        }
+#endif
+    }
+
+    public void Drag()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            dragging = false;
+        }
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragging = false;
+        }
+#endif
     }
 }
